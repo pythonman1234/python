32e160d4edf11f02402cb8438c5f6a75afc9147e
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2024-05-15 21:14:08 +0530

    Create Membership operators.py

---
+++ Membership operators.py
@@ -0,0 +1,8 @@
+# membership operator in Python
+# using "in" operator
+fruits = ["apple", "banana", "cherry"]
+if "banana" in fruits:
+    print("Yes, banana is a fruit!")
+# using "not in" operator
+if "orange" not in fruits:
+    print("Yes, orange is not in the list of fruits")

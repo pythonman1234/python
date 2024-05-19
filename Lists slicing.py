658a719f221aacc82a9a08877150dded73c4c986
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2024-05-19 18:12:57 +0530

    Create Lists slicing.py

---
+++ Lists slicing.py
@@ -0,0 +1,26 @@
+
+
+1. Basic Slicing:
+my_list = [1, 2, 3, 4, 5]
+sliced_list = my_list[1:4]
+# Output: [2, 3, 4]
+
+2. Negative Indexing:
+my_list = [1, 2, 3, 4, 5]
+sliced_list = my_list[-3:-1]
+# Output: [3, 4]
+
+3. Step Slicing:
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sliced_list = my_list[1:8:2]
+# Output: [2, 4, 6, 8]
+
+4. Slicing from the Start:
+my_list = [1, 2, 3, 4, 5]
+sliced_list = my_list[:3]
+# Output: [1, 2, 3]
+
+5. Slicing until the End:
+my_list = [1, 2, 3, 4, 5]
+sliced_list = my_list[2:]
+# Output: [3, 4, 5]
